02fd75ebb1db9db73ba19554291ed530e6d3c5be
sphnikola <sphnikola@gmail.com>
2024-01-18 03:58:42 +0000

    Add buttons to replace nav links for md view

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ function App() {
   console.log(data[0].name);
   return (
     <>
-      <main className="grid px-4 gap-y-8">
-        <section className="text-white">
-          <nav>
+      <main className="grid px-4 gap-y-8 md:grid-cols-2 md:gap-x-8">
+        <section className="text-white md:col-start-2">
+          <nav className="md:hidden">
             <ul className="flex justify-between gap-x-4">
               <li>
                 <a href="#" className=" focus:border-b-4 border-b-blue-400">
@@ -25,12 +25,23 @@ function App() {
               </li>
             </ul>
           </nav>
-          <hr className=" border-dotted bg-red-500" />
+          <hr className="md:hidden border-dotted bg-red-500" />
+          <div className="hidden md:block md:space-y-4 md:mt-5 ">
+            <button className="text-white border-2 border-solid w-full">
+              overview
+            </button>
+            <button className="text-white border-2 border-solid w-full">
+              structure
+            </button>
+            <button className="text-white border-2 border-solid w-full">
+              surface
+            </button>
+          </div>
         </section>
-        <section className="place-self-center ">
+        <section className="place-self-center md:row-start-1 md:col-start-1 md:col-end-3">
           <img src={mercury_img} />
         </section>
-        <section className="text-white text-center">
+        <section className="text-white text-center md:col-start-1 md:row-start-2 md:text-left">
           <h2 className="mb-3">{data[0].name}</h2>
           <p>{data[0].overview.content}</p>
         </section>
