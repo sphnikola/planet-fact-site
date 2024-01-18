9cc18c607a2f85f5e783cf0440026b5500123876
sphnikola <sphnikola@gmail.com>
2024-01-18 02:27:08 +0000

    Add background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-color": "hsl(240,67%,8%)",
+      },
+      backgroundImage: {
+        "bg-img": "url('./src/assets/background-stars.svg')",
+      },
+    },
   },
   plugins: [],
 };
