e9039670ae3671775afb2b722aaf60740b1ce66b
sphnikola <sphnikola@gmail.com>
2024-01-18 02:57:21 +0000

    Add nav items for sm screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
-import bg_img from "./assets/background-stars.svg";
 function App() {
   return (
     <>
-      <div></div>
+      <main className="grid">
+        <section className="text-white">
+          <nav>
+            <ul className="flex justify-between gap-x-4">
+              <li>
+                <a href="#" className=" focus:border-b-4 border-b-blue-400">
+                  overview
+                </a>
+              </li>
+              <li>
+                <a href="#" className=" focus:border-b-4 border-b-blue-400">
+                  Structure
+                </a>
+              </li>
+              <li>
+                <a href="#" className=" focus:border-b-4 border-b-blue-400">
+                  surface
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <hr className=" border-dotted bg-red-500" />
+        </section>
+      </main>
     </>
   );
 }
