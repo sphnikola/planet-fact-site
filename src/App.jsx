515191f13b7d41d550cede1c2c2ca049f9da2c43
sphnikola <sphnikola@gmail.com>
2024-01-19 01:21:15 +0000

    Improve on layout display

--- src/App.jsx
+++ src/App.jsx
@@ -44,32 +44,38 @@ function App() {
         <section className="text-white text-center md:col-start-1 md:row-start-2 md:text-left lg:col-start-3 lg:row-start-1">
           <h2 className="mb-3">{data[0].name}</h2>
           <p>{data[0].overview.content}</p>
+          <p className="text-white mt-8 lg:mt-16">
+            source:
+            <a href={data[0].overview.source} className="underline">
+              wikipedia
+            </a>
+          </p>
         </section>
       </main>
       <footer className="px-4 md:px-7 lg:max-w-6xl lg:mx-auto mt-10">
         <section className=" text-white flex flex-col gap-y-3 md:grid md:grid-cols-12 md:gap-x-1 lg:gap-x-8">
           <div className="md:col-start-1 md:col-end-4">
-            <div className="py-3 px-5 border-[1px] border-dotted flex justify-between md:flex-col  lg:gap-y-10 lg:p-4">
+            <div className="py-3 px-5 border-[1px] border-dotted flex justify-between items-center md:flex-col md:items-start md:gap-y-3 lg:gap-y-10 lg:p-4">
               <h3 className="text-xs">Rotation time</h3>
-              <p className=" text-4xl">53.6 days</p>
+              <p className="text-2xl md:text-3xl lg:text-4xl">53.6 days</p>
             </div>
           </div>
           <div className="md:col-start-4 md:col-end-7">
-            <div className="py-3 px-5 border-[1px] border-dotted flex justify-between md:flex-col lg:gap-y-10 lg:p-4">
+            <div className="py-3 px-5 border-[1px] border-dotted flex justify-between items-center md:flex-col md:items-start md:gap-y-3 lg:gap-y-10 lg:p-4">
               <h3 className="text-xs">Rotation time</h3>
-              <p className=" text-4xl">53.6 days</p>
+              <p className="text-2xl md:text-3xl lg:text-4xl">53.6 days</p>
             </div>
           </div>
           <div className="md:col-start-7 md:col-end-10">
-            <div className="py-3 px-5 border-[1px] border-dotted flex justify-between md:flex-col lg:gap-y-10 lg:p-4">
+            <div className="py-3 px-5 border-[1px] border-dotted flex justify-between items-center md:flex-col md:items-start md:gap-y-3 lg:gap-y-10 lg:p-4">
               <h3 className="text-xs">Rotation time</h3>
-              <p className=" text-4xl">53.6 days</p>
+              <p className="text-2xl md:text-3xl lg:text-4xl">53.6 days</p>
             </div>
           </div>
           <div className="md:col-start-10 md:col-end-13">
-            <div className="py-3 px-5 border-[1px] border-dotted flex justify-between md:flex-col lg:gap-y-10 lg:p-4">
+            <div className="py-3 px-5 border-[1px] border-dotted flex justify-between items-center md:flex-col md:items-start md:gap-y-3 lg:gap-y-10 lg:p-4">
               <h3 className=" text-xs">Rotation time</h3>
-              <p className=" text-4xl">53.6 days</p>
+              <p className="text-2xl md:text-3xl lg:text-4xl">53.6 days</p>
             </div>
           </div>
         </section>
