7c971e067985b6c1949b7d6d52b6a0ed29c2bd48
sphnikola <sphnikola@gmail.com>
2024-01-22 13:20:55 +0000

    Make hamburger show on md scren and fix nav logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,15 +7,15 @@ function NavBar() {
   return (
     <div>
       <nav className="w-full  scroll top-0 left-0 right-0 z-10">
-        <div className="justify-between px-4 mx-auto  md:items-center md:flex md:px-8 ">
+        <div className="justify-between px-4 mx-auto  lg:items-center lg:flex md:px-8 ">
           <div>
-            <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <div className="flex items-center justify-between py-3 lg:py-5 lg:block">
               {/* LOGO */}
               <a href="/">
-                <h2 className="text-2xl text-cyan-600 font-bold ">LOGO</h2>
+                <h2 className="text-2xl text-white font-bold ">THE PLANETS</h2>
               </a>
               {/* HAMBURGER BUTTON FOR MOBILE */}
-              <div className="md:hidden">
+              <div className="lg:hidden">
                 <button
                   className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
                   onClick={() => setNavbar(!navbar)}
@@ -37,47 +37,47 @@ function NavBar() {
           </div>
           <div>
             <div
-              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                navbar ? "p-12 md:p-0 block" : "hidden"
+              className={`flex-1 justify-self-center pb-3 mt-8 lg:block lg:pb-0 lg:mt-0 ${
+                navbar ? "p-12 lg:p-0 block" : "hidden"
               }`}
             >
-              <ul className="h-auto items-center justify-center text-xl md:text-base md:flex ">
-                <li className="pb-6  text-white py-2 px-6 text-left border-b-[0.2px] md:border-b-0    border-[rgb(55,55,78)]   ">
+              <ul className="h-auto items-center justify-center text-xl lg:text-base lg:flex ">
+                <li className="pb-6  text-white py-2 px-6 text-left border-b-[0.2px] lg:border-b-0    border-[rgb(55,55,78)]   ">
                   <a href="#about" onClick={() => setNavbar(!navbar)}>
                     Mercury
                   </a>
                 </li>
-                <li className="pb-6  text-white py-2 px-6 text-left border-b-[0.2px] md:border-b-0    border-[rgb(55,55,78)]   ">
+                <li className="pb-6  text-white py-2 px-6 text-left border-b-[0.2px] lg:border-b-0    border-[rgb(55,55,78)]   ">
                   <a href="#blog" onClick={() => setNavbar(!navbar)}>
                     Venus
                   </a>
                 </li>
-                <li className="pb-6  text-white py-2 px-6 text-left  border-b-[0.2px] md:border-b-0    border-[rgb(55,55,78)]   ">
+                <li className="pb-6  text-white py-2 px-6 text-left  border-b-[0.2px] lg:border-b-0    border-[rgb(55,55,78)]   ">
                   <a href="#contact" onClick={() => setNavbar(!navbar)}>
                     Earth
                   </a>
                 </li>
-                <li className="pb-6  text-white py-2 px-6 text-left  border-b-[0.2px] md:border-b-0    border-[rgb(55,55,78)]   ">
+                <li className="pb-6  text-white py-2 px-6 text-left  border-b-[0.2px] lg:border-b-0    border-[rgb(55,55,78)]   ">
                   <a href="#projects" onClick={() => setNavbar(!navbar)}>
                     Mars
                   </a>
                 </li>
-                <li className="pb-6  text-white py-2 px-6 text-left  border-b-[0.2px] md:border-b-0    border-[rgb(55,55,78)]   ">
+                <li className="pb-6  text-white py-2 px-6 text-left  border-b-[0.2px] lg:border-b-0    border-[rgb(55,55,78)]   ">
                   <a href="#projects" onClick={() => setNavbar(!navbar)}>
                     Jupiter
                   </a>
                 </li>
-                <li className="pb-6  text-white py-2 px-6 text-left  border-b-[0.2px] md:border-b-0    border-[rgb(55,55,78)]   ">
+                <li className="pb-6  text-white py-2 px-6 text-left  border-b-[0.2px] lg:border-b-0    border-[rgb(55,55,78)]   ">
                   <a href="#projects" onClick={() => setNavbar(!navbar)}>
                     Saturn
                   </a>
                 </li>
-                <li className="pb-6  text-white py-2 px-6 text-left  border-b-[0.2px] md:border-b-0    border-[rgb(55,55,78)]   ">
+                <li className="pb-6  text-white py-2 px-6 text-left  border-b-[0.2px] lg:border-b-0    border-[rgb(55,55,78)]   ">
                   <a href="#projects" onClick={() => setNavbar(!navbar)}>
                     Uranus
                   </a>
                 </li>
-                <li className="pb-6  text-white py-2 px-6 text-left  border-b-[0.2px] md:border-b-0    border-[rgb(55,55,78)]   ">
+                <li className="pb-6  text-white py-2 px-6 text-left  border-b-[0.2px] lg:border-b-0    border-[rgb(55,55,78)]   ">
                   <a href="#projects" onClick={() => setNavbar(!navbar)}>
                     Neptune
                   </a>
